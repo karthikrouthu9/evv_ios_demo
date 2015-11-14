f27f8877fc1f5e53757898ef358dbec8dd29db51
karthikrouthu9 <routhukarthik93@gmail.com>
2015-11-13 23:40:03 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,8 +22,9 @@
 
 		function onDeviceReady() {
 			alert("Device Ready");
+			window.plugins.uniqueDeviceID.get(success, fail);
  		var element = document.getElementById('deviceProperties');
-		var device_uuid = device.uuid;
+		//var device_uuid = device.uuid;
        
  	var networkState = navigator.connection.type;
     if (networkState == Connection.NONE)
@@ -79,8 +80,8 @@ alert("Before Ajax");
           },
           success: function (token) {   
 		alert("Success Token func");
-   	var device_uuid = device.uuid;
-	var d = document.getElementById("device_uuid");
+   	//var device_uuid = device.uuid;
+	//var d = document.getElementById("device_uuid");
 	var token =token;
 	var header = "X-CSRF-TOKEN";
     $(document).ajaxSend(function(e, xhr, options) {
@@ -93,6 +94,9 @@ alert("Before Ajax");
    
  function is_device_registered(device_uuid)
 {
+	var device_uuid = globalVariable;
+	alert("For Device Value");
+	alert(device_uuid);
 	             $.ajax({
               url: 'http://183.82.96.212:8080/m_service/m_resources/is_device_registered',
               type: "post",
@@ -143,10 +147,10 @@ alert("Before Ajax");
   }
 });
               },
-			                success: function (data) {
+	success: function (data) {
 							
-			                	//data.logindata[0].is_security_question_answered;
-							if(data.is_security_question_answered==1 && data.logindata[0].count>=1)
+	//data.logindata[0].is_security_question_answered;
+	if(data.is_security_question_answered==1 && data.logindata[0].count>=1)
       {
    	alert("Already Registered");
       window.location='./log-in.html';
@@ -174,4 +178,21 @@ alert("Before Ajax");
 			});
 }
 }
+    
+    var globalVariable;			
+     function success(uuid)		
+{		
+    alert(uuid);		
+   		
+		globalVariable=uuid;
+		is_device_registered(device_uuid)
+};		
+function fail(uuid)		
+{		
+   		
+    alert("failure function reg");		
+};			
+    			
+    			
+    			
     			
